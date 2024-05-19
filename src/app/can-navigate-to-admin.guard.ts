148c0a00974d5d12fc0239e98ed4a5c557f605b9
mariehilmer <s2110456011@fhooe.at>
2024-05-19 22:21:40 +0200

    FE adaptations (logged out user view)

--- src/app/can-navigate-to-admin.guard.ts
+++ src/app/can-navigate-to-admin.guard.ts
@@ -5,13 +5,15 @@ import {inject} from "@angular/core";
 export const canNavigateToAdminGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthenticationService);
   const router = inject(Router);
+  console.log("maybe here?")
   if (authService.isLoggedIn()) {
     return true;
+    console.log("not errror")
   } else {
-    window.alert(
-      "Du musst Dich einloggen, um den Administrationsbereich zu betreten"
-    );
+    window.alert("Du musst Dich einloggen, um den Administrationsbereich zu betreten");
     console.log(state);
+    console.log("Window", window);
+    console.log("error");
     router.navigate(["../"]);
     return false;
   }
